5a9fea4c086c00e358237c80619476cdc2cdff04
t1meal <t1meal@yandex.ru>
2021-09-30 18:49:59 +0300

    делаю...

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Задание 1
+        String [] array = {"abc1", "dfa2", "egc3", "fbd4"};
+        System.out.println("Original array\n: " + Arrays.toString(array));
+        swapElement(array,0, 1);
+        System.out.println("Changed array\n: " + Arrays.toString(array));
+
+
+    }
+    public static <T> void swapElement(T[] array, int e1, int e2) {
+        T temp = array [e1];
+        array[e1] = array[e2];
+        array [e2] = temp;
+    }
+}
