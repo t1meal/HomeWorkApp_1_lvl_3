1915caa404612d9f0b29f10bc4d84a99016a27f3
t1meal <t1meal@yandex.ru>
2021-09-30 22:19:03 +0300

    выполнил задание просьба проверить

---
+++ src/Fruit.java
@@ -0,0 +1,11 @@
+public class Fruit {
+    public final float weight;
+
+    public Fruit(float weight) {
+        this.weight = weight;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+}
